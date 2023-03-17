6002800ff2904d485324922b14cbf61a3d372d9e
santoshr <san_rai@hotmail.com>
2023-03-17 09:49:22 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ghumoo-ionic-pwa-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
